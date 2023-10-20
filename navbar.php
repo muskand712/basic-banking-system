9bfc2ee1321c4014f3e41e99dc8c1ed691fd722f
muskand712 <117376035+muskand712@users.noreply.github.com>
2023-10-20 19:48:08 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,25 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md bg-secondary navbar-dark">
+      <a class="navbar-brand" href="index.php" style="color:black"><img src="img/My bank logo.png" alt="Logo" width="300" style="margin-bottom:4px;"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : black;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : black;"><b>Create a User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="removeuser.php" style="color : black;"><b>Remove user</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : black;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : black;"><b>Transaction history</b></a>
+              </li>
+          </div>
+</nav>
